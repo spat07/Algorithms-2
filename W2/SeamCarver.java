6f6b0a344e84f9b4453358c86595d490ea140485
SP <50160270+spat07@users.noreply.github.com>
2021-03-08 13:07:26 +0530

    New seam finding implementation

--- W2/SeamCarver.java
+++ W2/SeamCarver.java
@@ -1,24 +1,30 @@
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
+//import edu.princeton.cs.algs4.StdOut;
 
 public class SeamCarver {
 	
 	private Picture myImage;
 	private double [][] imgEnrg;
-	private int H, W;
+	private double [][] pathEnrg;
+	private int [][] edgeTo;
 
    // create a seam carver object based on the given picture
    public SeamCarver(Picture picture) {
 	   
 	   if (picture == null)
 		   throw new IllegalArgumentException("Invalid Input");
+
+	   int H, W;
 	   
 	   myImage = new Picture(picture);
 	   
 	   H = picture.height();
 	   W = picture.width();
 	   
-	   StdOut.printf("height = %d, width = %d\n", H, W);
+//	   StdOut.printf("height = %d, width = %d\n", H, W);
 	   
 	   imgEnrg = new double[H][W];
 	   
@@ -51,7 +57,7 @@ public class SeamCarver {
    // energy of pixel at column x and row y
    public double energy(int c, int r) {
 	   
-	   if(r == 0 || r == (H - 1) || c == 0 || c == (W - 1)) {
+	   if(r == 0 || r == (height() - 1) || c == 0 || c == (width()- 1)) {
 		   return 1000.00;
 	   }
 	   
@@ -75,137 +81,135 @@ public class SeamCarver {
 	   return (Math.sqrt(xGrad + yGrad));
    }
 
-   // sequence of indices for horizontal seam
-   public int[] findHorizontalSeam() {
-	   // start from r = 1 to H-1
-	   // for all vertices in height
-	   // 	find the imgEnrg[r+1, (c-1, c, c+1)] with minimizing energySum
-	   //   add it to the list
-	   
-	   int [] vList = new int[W];
-	   int [] tempvList = new int[W];
-	   double enrgSum = Double.MAX_VALUE;
-	   
-	   for(int r = 1 ; r < H - 1; r++) {
+   private void relax (int vc, int vr, int wc, int wr, double[][] enrg) {
+	   if(pathEnrg[wr][wc] > pathEnrg[vr][vc] + enrg[wr][wc]) {
+		   pathEnrg[wr][wc] = pathEnrg[vr][vc] + enrg[wr][wc];
+		   edgeTo[wr][wc] = vc;
+	   }
+   }
+   
+   private int[] findSeam(double[][] enrg, int H, int W) {
+	   for(int r = 0; r < H - 1; r++) {
+		   for(int c = 0; c < W; c++) {
 
-		   int r_ = r;
-		   double tempEnrgSum = 0;
-		   
-		   int c = 0;
-		   tempvList[c] = r_;
-		   tempEnrgSum += imgEnrg[r_][c];
-		   
-		   for(c = 1; c < W - 1; c++) {
-//			   StdOut.printf("ie[%d][%d]=%f, ie[%d][%d]=%f, ie[%d][%d]=%f, ", 
-//					   r_ - 1, c, imgEnrg[r_ - 1][c],
-//					   r_, c, imgEnrg[r_][c],
-//					   r_ + 1, c, imgEnrg[r_ + 1][c]);
-			   
-			   if (imgEnrg[r_ - 1][c] < imgEnrg[r_][c]) { // a < b
-				   
-				   if(imgEnrg[r_ - 1][c] < imgEnrg[r_ + 1][c]) { // a < c
-					   r_ = r_ - 1;	// a is least
-				   } else {
-					   r_ = r_ + 1; // c is least (a < b and a > c => c < b as well)
-				   }
-			   } else { // a > b
-				   if (imgEnrg[r_][c] < imgEnrg[r_ + 1][c]) { // b < c
-					   // no change - b is least
-				   } else { // c < b
-					   r_ = r_ + 1; // c is least (a > b and b > c)
-				   }
+			   // relax c - 1, r + 1 (if c > 0)
+			   if(c > 0 ) {
+				   relax(c, r, c - 1, r + 1, enrg);
 			   }
 			   
-			   
-			   tempvList[c] = r_;
-			   tempEnrgSum += imgEnrg[r_][c];
-//			   StdOut.printf("tempvList[%d]=%d, tempEnrgSum=%f, enrgSum = %f\n", c, tempvList[c], tempEnrgSum, enrgSum);
+			   // relax c, r+1
+			   relax(c, r, c , r + 1, enrg);
+
+
+			   // relax c + 1, r + 1 (if c < W)
+			   if(c < W - 1) {
+				   relax(c, r, c + 1, r + 1, enrg); 
+			   }
+
 		   }
-		   
-		   tempvList[c] = r_ - 1;
-		   tempEnrgSum += imgEnrg[r_][c];
-		   
-//		   StdOut.printf("\ntempEnrgSum=%f, enrgSum=%f, c=%d, tempvList=", tempEnrgSum, enrgSum, c);
-//		   for(int i : tempvList) {
-//			   StdOut.printf("%d,", i);
-//		   }
-		   if(tempEnrgSum < enrgSum) {
-			   System.arraycopy(tempvList, 0, vList, 0, W);
-			   enrgSum = tempEnrgSum;
+				   
+	   }
+	   
+	   // find min energy index
+	   double minEnrg = Double.POSITIVE_INFINITY;
+	   int minIdx = 0;
+	   for(int c = 0; c < W; c++) {
+		   if(pathEnrg[H-1][c] < minEnrg) {
+			   minEnrg = pathEnrg[H-1][c];
+			   minIdx = c;
 		   }
 	   }
 	   
-	   return vList;
+	   // now find path walking back from minIdx in edge to
+	   int seam[] = new int[H];
+	   seam[H-1] = minIdx;
+	   for(int r = H - 2; r >= 0; r--) {
+		   seam[r] = edgeTo[r+1][seam[r+1]];
+	   }
+	   
+//	   //test code
+//	   StdOut.print("Path Energies:\n");
+//	   for(int r = 0; r < H; r++) {
+//		   for(int c = 0; c < W; c++) {
+//			   StdOut.printf("%.2f,", pathEnrg[r][c]);
+//		   }
+//		   StdOut.print("\n");
+//	   }
+//	   
+//	   StdOut.print("Path:\n");
+//	   for(int r = 0; r < H; r++) {
+//		   for(int c = 0; c < W; c++) {
+//			   StdOut.printf("%d,", edgeTo[r][c]);
+//		   }
+//		   StdOut.print("\n");
+//	   }
+	   
+//	   StdOut.print("Print seam:\n");
+//	   for(int r = 0; r < H; r++) {
+//		   StdOut.printf("%d, ", seam[r]);
+//	   }
+//	   StdOut.print("\n");
+	   
+	   return seam;
+	   
+   }
+
+   private void setup(int H, int W) {
+	   pathEnrg = new double[H][W];
+	   edgeTo = new int[H][W];
+	   
+	   for(int r = 0 ; r < H; r++) {
+		   for (int c = 0; c < W; c++) {
+			   pathEnrg[r][c] = Double.POSITIVE_INFINITY;
+			   edgeTo[r][c] = 0;
+		   }
+
+	   }
+	   
+	   for(int c = 0; c < W; c++) {
+		   pathEnrg[0][c] = 0;
+	   }
+
+   }
+   // sequence of indices for horizontal seam
+   public int[] findVerticalSeam() {
+	   setup(height(), width());
+	   int seam[] = findSeam(imgEnrg, height(), width());
+	   return seam;
 
    }
 
-   // sequence of indices for vertical seam
-   public int[] findVerticalSeam() {
+   private double[][] transposeImgEnrg() {
+	   //reversed dims
+	   int H = width();
+	   int W = height();
 	   
-	   // start from c = 0 to W-1
-	   // for all vertices in height
-	   // 	find the imgEnrg[r+1, (c-1, c, c+1)] with minimizing energySum
-	   //   add it to the list
+	   double [][] enrg = new double[H][W]; //reversed dims
 	   
-	   int [] vList = new int[H];
-	   int [] tempvList = new int[H];
-	   double enrgSum = Double.MAX_VALUE;
 	   
-	   for(int c = 1 ; c < W - 1; c++) {
-
-		   int c_ = c;
-		   double tempEnrgSum = 0;
-		   
-		   int r = 0;
-		   tempvList[r] = c_;
-		   tempEnrgSum += imgEnrg[r][c_];
-		   
-		   for(r = 1; r < H - 1; r++) {
-//			   StdOut.printf("ie[%d][%d]=%f, ie[%d][%d]=%f, ie[%d][%d]=%f, ", 
-//					   r, c_ -1, imgEnrg[r][c_ - 1],
-//					   r, c_, imgEnrg[r][c_],
-//					   r, c_ + 1, imgEnrg[r][c_ + 1]);
-			   
-			   if (imgEnrg[r][c_ - 1] < imgEnrg[r][c_]) { // a < b
-				   
-				   if(imgEnrg[r][c_ - 1] < imgEnrg[r][c_ + 1]) { // a < c
-					   c_ = c_ - 1;	// a is least
-				   } else {
-					   c_ = c_ + 1; // c is least (a < b and a > c => c < b as well)
-				   }
-			   } else { // a > b
-				   if (imgEnrg[r][c_] < imgEnrg[r][c_ + 1]) { // b < c
-					   // no change - b is least
-				   } else { // c < b
-					   c_ = c_ + 1; // c is least (a > b and b > c)
-				   }
-			   }
-			   
-			   tempvList[r] = c_;
-			   tempEnrgSum += imgEnrg[r][c_];
-//			   StdOut.printf("tempvList[%d] = %d, tempEnrgSum = %f, enrgSum = %f\n", r, tempvList[r], tempEnrgSum, enrgSum);
-		   }
-		   
-		   tempvList[r] = c_ - 1;
-		   tempEnrgSum += imgEnrg[r][c_];
-		   
-//		   StdOut.printf("\ntempEnrgSum=%f, enrgSum=%f, c=%d, tempvList=", tempEnrgSum, enrgSum, c);
-//		   for(int i : tempvList) {
-//			   StdOut.printf("%d,", i);
-//		   }
-		   if(tempEnrgSum < enrgSum) {
-			   System.arraycopy(tempvList, 0, vList, 0, H);
-			   enrgSum = tempEnrgSum;
+	   for(int r = 0; r < H; r++) {
+		   for(int c = 0; c < W; c++) {
+			   enrg[r][c] = imgEnrg[c][r];
 		   }
 	   }
 	   
-	   return vList;
+	   return enrg;
+   }
+   // sequence of indices for vertical seam
+   public int[] findHorizontalSeam() {
+	   //transpose the imgEnrg and find vertical seam
+	   double tpImgEngrg[][] = transposeImgEnrg();
+
+	   //we exchanged dims as transpose
+	   setup(width(), height());
+	   int seam[] = findSeam(tpImgEngrg, width(), height());
+	   return seam;
    }
 
    // remove horizontal seam from current picture
    public void removeHorizontalSeam(int[] seam) {
 	   
-	   if (seam == null || seam.length != W)
+	   if (seam == null || seam.length != width())
 		   throw new IllegalArgumentException("Invalid Input");
 
 	   for(int r = 1; r < seam.length; r++) {
@@ -214,7 +218,9 @@ public class SeamCarver {
 		   }
 	   }
 	   
-	   H = myImage.height() - 1;
+	   int H = myImage.height() - 1;
+	   int W = width();
+	   
 	   Picture modPic = new Picture(W , H); // remove one horizontal pixel line
 
 	   
@@ -244,7 +250,7 @@ public class SeamCarver {
    // remove vertical seam from current picture
    public void removeVerticalSeam(int[] seam) {
 	   
-	   if (seam == null || seam.length != H)
+	   if (seam == null || seam.length != height())
 		   throw new IllegalArgumentException("Invalid Input");
 	   
 	   for(int c = 1; c < seam.length; c++) {
@@ -253,7 +259,9 @@ public class SeamCarver {
 		   }
 	   }
 	   
-	   W = myImage.width() - 1;
+	   int W = myImage.width() - 1;
+	   int H = height();
+	   
 	   Picture modPic = new Picture(W, H); // remove one horizontal pixel line
 	   
 	   int rIn, cIn, rOut, cOut;
@@ -294,8 +302,15 @@ public class SeamCarver {
    //  unit testing (optional)
    public static void main(String[] args) {
 	   
-//	   Picture testImg = new Picture(args[0]);
-//	   SeamCarver sc1 = new SeamCarver(testImg);
+	   Picture testImg = new Picture(args[0]);
+	   SeamCarver sc1 = new SeamCarver(testImg);
+	   
+	   StdOut.print("find vertical seam\n");
+	   sc1.findVerticalSeam();
+	   
+	   StdOut.print("find horizontal seam\n");
+	   sc1.findHorizontalSeam();
+	   
 //	   Picture pe = sc1.pictureEnergies();
 //	   pe.save(args[1]);
 	   
